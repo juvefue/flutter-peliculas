a0772b820dee4279372bd1fb82f3939244c2036e
Juvencio Nolasco <gospa.apps@gmail.com>
2020-07-23 17:16:41 -0500

    widget Personalizado

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/src/widgets/card_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -6,7 +7,25 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Hola peliculas'),
+      appBar: AppBar(
+        title: Text('Peliculas en cines'),
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.search), onPressed: null)
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[_swipperTarjetas()],
+        ),
+      ),
     );
+  }
+
+  Widget _swipperTarjetas() {
+    return CardSwiper(
+      peliculas: [1, 2, 3, 4, 5],
+    );
+    //return Container();
   }
 }
