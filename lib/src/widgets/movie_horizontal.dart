8f30420bb187b7534f2191ea30b479fbb438d9fb
Juvencio Nolasco <gospa.apps@gmail.com>
2020-07-28 18:56:21 -0500

    Stream builder con optimizaciones

--- lib/src/widgets/movie_horizontal.dart
+++ lib/src/widgets/movie_horizontal.dart
@@ -15,9 +15,10 @@ class MovieHorizontal extends StatelessWidget {
     final _screenSize = MediaQuery.of(context).size;
 
     _pageController.addListener(() {
+      //siguientePagina();
+
       if (_pageController.position.pixels >=
           _pageController.position.maxScrollExtent - 200.0) {
-        print('siguiente pagina');
         siguientePagina();
       }
     });
@@ -60,29 +61,30 @@ class MovieHorizontal extends StatelessWidget {
     );
   }
 
-  List<Widget> _tarjetas(BuildContext context) {
-    return peliculas.map((pelicula) {
-      return Container(
-        margin: EdgeInsets.only(right: 15.0),
-        child: Column(
-          children: <Widget>[
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
-              child: FadeInImage(
-                placeholder: AssetImage('assets/img/no-image.jpg'),
-                image: NetworkImage(pelicula.getPosterImg()),
-                fit: BoxFit.cover,
-                height: 110.0,
-              ),
-            ),
-            Text(
-              pelicula.title,
-              overflow: TextOverflow.ellipsis,
-              style: Theme.of(context).textTheme.caption,
-            )
-          ],
-        ),
-      );
-    }).toList();
-  }
+  // List<Widget> _tarjetas(BuildContext context) {
+  //   return peliculas.map((pelicula) {
+  //     return Container(
+  //       margin: EdgeInsets.only(right: 15.0),
+  //       child: Column(
+  //         children: <Widget>[
+  //           ClipRRect(
+  //             borderRadius: BorderRadius.circular(20.0),
+  //             child: FadeInImage(
+  //               placeholder: AssetImage('assets/img/no-image.jpg'),
+  //               image: NetworkImage(pelicula.getPosterImg()),
+  //               fit: BoxFit.cover,
+  //               height: 110.0,
+  //             ),
+  //           ),
+  //           Text(
+  //             pelicula.title,
+  //             overflow: TextOverflow.ellipsis,
+  //             style: Theme.of(context).textTheme.caption,
+  //           )
+  //         ],
+  //       ),
+  //     );
+  //   }).toList();
+  // }
+
 }
